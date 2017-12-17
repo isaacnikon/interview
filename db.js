5578e73226678687c8ff7ffa5fdef270f99ce685
root <root@server.nikon93.xyz>
2017-12-17 16:50:53 +0000

    working of sorts

--- db.js
+++ db.js
@@ -11,21 +11,42 @@ const client = new pg.Client({
 client.connect();
 
 var addThreads = function(threads, email) {
-  client.query(`delete from threads where email='${email}';`, function(err, res) {
-    if(!err){
-      console.log("Successful delete");
-    }else {
+
+  client.query(`delete from messages where threadid in (select threadid from threads where email='${email}');`, function(err, res) {
+    if (!err) {
+      console.log("Successful messages delete");
+    } else {
       console.log(err);
     }
   });
-  threads.forEach(function(thread){
-    client.query(`insert into threads(email,threadid) values('${email}','${thread.id}')`,function(err,res){
-      if(!err){
-        console.log("Successful addThreads");
-      }else {
+  client.query(`delete from threads where email='${email}';`, function(err, res) {
+    if (!err) {
+      console.log("Successful delete");
+    } else {
+      console.log(err);
+    }
+  });
+  threads.forEach(function(thread) {
+    client.query(`insert into threads(email,threadid) values('${email}','${thread.id}')`, function(err, res) {
+      if (!err) {
+        // console.log("Successful addThreads");
+      } else {
         console.log(err);
       }
     });
   });
 }
-module.exports.addThreads=addThreads;
+module.exports.addThreads = addThreads;
+
+var addMessages = function(messages) {
+  messages.forEach(function(thread) {
+    client.query(`insert into messages(messagesid,threadid) values('${message.id}','${thread.id}')`, function(err, res) {
+      if (!err) {
+        console.log("Successful messages");
+      } else {
+        console.log(err);
+      }
+    });
+  });
+}
+module.exports.addMessages = addMessages;
